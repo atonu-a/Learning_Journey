735e0c1d8441df5839d1bfa403ebabd9ae5b99a0
Atonu <susnigdho1234@gmail.com>
2026-09-04 13:25:27 +0600

    Comment out unused variables and code for clarity

--- Module 1/m01.py
+++ Module 1/m01.py
@@ -9,7 +9,18 @@
 # a = str(input())
 # print(type(a))
 
-a= 5
-b =7
+# a= 5
+# b =7
 
-print(a==b)
+# print(a==b)
+
+# counter = 1
+# while counter<=10:
+#     print(counter)
+#     counter+=1
+    
+
+
+# list
+marks =[1, 4, 3, 5,3 ,3]
+print(marks[-3:])
