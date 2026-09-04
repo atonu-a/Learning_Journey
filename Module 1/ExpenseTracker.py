9eb3a36fbe2412b907bd363ea59347518420aaaf
Atonu <susnigdho1234@gmail.com>
2026-09-04 12:06:55 +0600

    Refactor Expense Tracker input handling for clarity and usability

--- Module 1/ExpenseTracker.py
+++ Module 1/ExpenseTracker.py
@@ -2,16 +2,20 @@ print("== Welcome to Expense Tracker ==")
 
 balance = 0
 
-transaction = input("expense or income ? :")
-if (transaction == "expense"):
-    taka = int(input("How much ? : "))
-    balance -= taka
-    print(f"Your current balance is {balance} tk.")
-elif(transaction == "income"):
-    taka = int(input("How much ? : "))
-    balance += taka
-    print(f"Your current balance is {balance} tk.")
-else:
-    print("Please check your input and try again!")
 
-print("== Thank Your For Using! ==")
+while True:
+    transaction = input("Enter e for expense, i for income and b for balance :")
+    if (transaction == "e"):
+        taka = int(input("How much ? : "))
+        balance -= taka
+        print(f"Your current balance is {balance} tk.")
+    elif(transaction == "i"):
+        taka = int(input("How much ? : "))
+        balance += taka
+        print(f"Your current balance is {balance} tk.")
+    elif(transaction == "b"):
+        print(f"Your current balance is {balance} tk.")
+    else:
+        print("Please check your input and try again!")
+
+    print("== Thank Your For Using! ==")
