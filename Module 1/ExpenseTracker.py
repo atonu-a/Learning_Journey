93593d518629d40877b0ae73712cdce82082e1b7
Atonu <susnigdho1234@gmail.com>
2026-09-04 12:19:03 +0600

    Improve Expense Tracker input handling and add exit option

--- Module 1/ExpenseTracker.py
+++ Module 1/ExpenseTracker.py
@@ -4,18 +4,23 @@ balance = 0
 
 
 while True:
-    transaction = input("Enter e for expense, i for income and b for balance :")
-    if (transaction == "e"):
-        taka = int(input("How much ? : "))
-        balance -= taka
-        print(f"Your current balance is {balance} tk.")
-    elif(transaction == "i"):
-        taka = int(input("How much ? : "))
-        balance += taka
-        print(f"Your current balance is {balance} tk.")
-    elif(transaction == "b"):
-        print(f"Your current balance is {balance} tk.")
-    else:
-        print("Please check your input and try again!")
+    transaction = input("Enter e for expense, i for income, b for balance and exit for exit : ")
+    if (balance<=0 and transaction == "e"):
+        print("You don't have sufficient balance to expense! ")
+    else:       
+        if (transaction == "e"):
+            taka = int(input("How much ? : "))
+            balance -= taka
+            print(f"Your current balance is {balance} tk.")
+        elif(transaction == "i"):
+            taka = int(input("How much ? : "))
+            balance += taka
+            print(f"Your current balance is {balance} tk.")
+        elif(transaction == "b"):
+            print(f"Your current balance is {balance} tk.")
+        elif(transaction == "exit"):
+            break
+        else:
+            print("Please check your input and try again!")
 
-    print("== Thank Your For Using! ==")
+print("== Thank Your For Using! ==")
