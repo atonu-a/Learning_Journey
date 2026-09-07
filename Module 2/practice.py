5c74c3f12a2dd450acb0ec6e25e244134ccab319
Atonu <susnigdho1234@gmail.com>
2026-09-07 12:55:07 +0600

    Refactor function definitions and improve code formatting for clarity

--- Module 2/practice.py
+++ Module 2/practice.py
@@ -7,7 +7,16 @@ def first_function():
 first_function()
 
 # Input, no return
-def addition(a, b):
+def addition(a, b): #arguments
     print(a+b)
     
-addition(10,15)
+addition(10,15) #parameters
+
+# Input and return
+
+def multiplication(a, b):
+    return a*b
+
+result = multiplication(10, 20)
+print(result)
+
