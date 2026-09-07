f8b3c9351d3e0427a0b1561e9e4ece6661c0655e
Atonu <susnigdho1234@gmail.com>
2026-09-07 13:06:45 +0600

    Add addition function to handle multiple arguments and create key_arguments.py file

---
+++ Module 2/arguments.py
@@ -0,0 +1,6 @@
+# Multiple arguments
+def addition(*args): 
+    #args diye amra eksathe multiple arguments nite pari. kono fixed nai je etoguloi nite hobe
+
+    return sum(args)
+print(addition(10, 15, 20))
