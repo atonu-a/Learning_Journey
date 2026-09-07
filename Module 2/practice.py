1d75305bfce26ab5d3548a299fbfd204ed67dff2
Atonu <susnigdho1234@gmail.com>
2026-09-07 23:43:23 +0600

    Refactor practice.py to improve code organization and add decorator.py file

--- Module 2/practice.py
+++ Module 2/practice.py
@@ -1,27 +1,36 @@
-# User defined functions
-# 1. No input, no return
+# # User defined functions
+# # 1. No input, no return
 
-def first_function():
-    print("this is my first function")
+# def first_function():
+#     print("this is my first function")
     
-first_function()
+# first_function()
 
-# 2. Input, no return
-def addition(a, b): #arguments
-    print(a+b)
+# # 2. Input, no return
+# def addition(a, b): #parameters
+#     print(a+b)
     
-addition(10,15) #parameters
+# addition(10,15) #arguments
 
-# 3. Input and return
+# # 3. Input and return
 
-def multiplication(a, b):
-    return a*b
+# def multiplication(a, b):
+#     return a*b
 
-result = multiplication(10, 20)
-print(result)
+# result = multiplication(10, 20)
+# print(result)
 
-# 4. No input, return
+# # 4. No input, return
 
-def greetings():
-    return "Hello"
-print(greetings())
+# def greetings():
+#     return "Hello"
+# print(greetings())
+
+# def sum(a, b):
+#     res = a+b
+# print(sum)
+# print(sum(10,15))
+
+calc = lambda a, b : a+b
+
+print(calc(10, 20))
