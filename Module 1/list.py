f94eadf0aa5a9f306afe1f2d4b001bfd6ab84747
Atonu <susnigdho1234@gmail.com>
2026-09-08 13:43:52 +0600

    Lists added

---
+++ Module 1/list.py
@@ -0,0 +1,17 @@
+"""a = [1, 10, 23, 24, 26, 90]
+
+# To calculate length of a list
+print(len(a)) #6
+
+# Append in list
+a.append(100)
+print(a) #[1, 10, 23, 24, 26, 90, 100]
+
+# Clear list
+print(a.clear()) #None
+
+# Convert a string into a list
+
+s = "Hello"
+print(list(s)) #['H', 'e', 'l', 'l', 'o']"""
+   
