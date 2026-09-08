fb3c90f44945c9d0d2fcdde18ac98dddb5349c3e
Atonu <susnigdho1234@gmail.com>
2026-09-08 14:01:16 +0600

    range() function learnd

--- Module 1/list.py
+++ Module 1/list.py
@@ -15,3 +15,18 @@ print(a.clear()) #None
 s = "Hello"
 print(list(s)) #['H', 'e', 'l', 'l', 'o']"""
    
+
+# Range method
+# range (start, end, step)
+# range(end)
+# range (start, end)
+
+a = list(range(10)) # range(end)
+#[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(a) #[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+a = list(range(1, 10))
+print(a) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+a = list(range(0, 10, 2))
+print(a) #[1, 2, 3, 4, 5, 6, 7, 8, 9]
---
+++ Module 1/tuples.py
@@ -0,0 +1,4 @@
+a = (100, 200, 300)
+
+print(tuple(reversed(a)))
+print(a)
