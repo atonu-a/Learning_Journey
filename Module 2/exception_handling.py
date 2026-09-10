85516388cc08e7ddc59b4cc9186ce56a2c5956fe
Atonu <susnigdho1234@gmail.com>
2026-09-10 19:41:16 +0600

    Refactor exception handling example and add file type validation

--- Module 2/exception_handling.py
+++ Module 2/exception_handling.py
@@ -1,4 +1,4 @@
-try : #Prothome try korbe
+"""try : #Prothome try korbe
     a = int(input("Enter a : "))
     b = int(input("Enter b : "))
     div = a/b
@@ -9,4 +9,14 @@ else : #try te kono error na thakle eta run hobe
     print("Code executed successfully!") 
 finally : # always eta run hobeiiiii
     print("Done")
+    """
     
+# Manually Error toiri ba raise kora
+def check_file(filename):
+    if not filename.endswith('.txt'):
+        raise ValueError("Only .txt files are accepted!") #Ekhane manuall vabe ekta error/exception raise kora holo. Ekhane jekono error deya jay chaile
+    else:
+        print("File uploaded successfully!")
+        
+check_file("name.txt")      
+check_file("name.png")
