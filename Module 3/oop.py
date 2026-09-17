13bedc9228112d8c9bb8c395ccad88c48bbda631
Atonu <susnigdho1234@gmail.com>
2026-09-17 21:15:20 +0600

    Refactor Phone class to include default parameters and update example usage

--- Module 3/oop.py
+++ Module 3/oop.py
@@ -1,7 +1,18 @@
-class Car:
-    def __init__(self, brand, model) :
+class Phone:
+    def __init__(self, brand, model) : #Parameterized Constructor
         self.brand = brand
         self.model = model
+        
+    def __init__(self, brand="Apple" , model ="18"):#Default Parameter
+        self.brand = brand
+        self.model = model
+    
 
-bike = Car("Royal", "B6")
-print(bike.model)
+phone1 = Phone("Oppo", "S6")
+
+phone2 = Phone(brand="Samsung", model="S26")
+
+phone3 = Phone()
+print(phone1.brand)
+print(phone2.brand)
+print(phone3.brand)
