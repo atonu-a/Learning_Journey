8a4513af61135a2bde8cec5c58471000bfea20b6
Atonu <susnigdho1234@gmail.com>
2026-09-21 23:30:36 +0600

    Implement Singleton design pattern for DBConnection class

--- Module 3/desgin_patterns.py
+++ Module 3/desgin_patterns.py
@@ -1,1 +1,17 @@
 #Singleton design pattern
+
+
+
+class DBConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+a = DBConnection()
+b = DBConnection()
+c = DBConnection()
+
+print(a._instance) #without new method , the both objects are different
